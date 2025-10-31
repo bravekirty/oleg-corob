83cb69230d92ae215be3eabcd6a4f7de95d52121
Kir Tarasov <kir_tarasov@mail.ru>
2025-10-31 18:09:22 +0300

    feat: add document view

--- humanrights_site/documents/urls.py
+++ humanrights_site/documents/urls.py
@@ -5,6 +5,4 @@ from documents import views
 app_name = 'documents'
 urlpatterns = [
     path('', views.AllAttachmentsListView.as_view(), name='all-attachments'),
-    path('attachment/<int:pk>/', views.AttachmentDetailView.as_view(), name='attachment_detail'),
-    path('attachment/<int:pk>/view/', views.attachment_file_view, name='attachment_view'),
 ]
--- humanrights_site/documents/views.py
+++ humanrights_site/documents/views.py
@@ -8,53 +8,28 @@ import documents.models
 
 class AllAttachmentsListView(django.views.generic.ListView):
     model = documents.models.Attachment
-    template_name = 'documents/all_attachments_list.html'  # You can customize this
-    context_object_name = 'attachments'  # This will be the variable name in your template
-    paginate_by = 20  # Optional: add pagination with 20 items per page
+    template_name = 'documents/all_attachments_list.html'
+    context_object_name = 'attachments'
+    paginate_by = 20
 
     def get_queryset(self):
         queryset = super().get_queryset()
-
-        # Add file size information to each attachment
-        #queryset = queryset.select_related('article')
-
-        return queryset.order_by('-id')
+        return queryset.select_related('article').order_by('-id')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['pageTitle'] = "Documents"
+
+        # Get the file ID from URL parameter
+        file_id = self.request.GET.get('file')
+        if file_id:
+            try:
+                attachment = documents.models.Attachment.objects.get(id=file_id)
+                context['open_file_id'] = attachment.id
+                context['open_file_url'] = attachment.file.url
+                context['open_file_name'] = attachment.name
+                context['open_file_extension'] = os.path.splitext(attachment.file.name)[1].lower()
+            except documents.models.Attachment.DoesNotExist:
+                pass
+
         return context
-
-
-class AttachmentDetailView(django.views.generic.DetailView):
-    model = documents.models.Attachment
-    template_name = 'documents/attachment_detail.html'
-    context_object_name = 'attachment'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['pageTitle'] = f"Document - {self.object.name}"
-        return context
-
-
-def attachment_file_view(request, pk):
-    """
-    View to display attachment file in the PDF viewer
-    """
-    try:
-        attachment = documents.models.Attachment.objects.get(pk=pk)
-
-        # Check if it's a PDF file
-        file_extension = os.path.splitext(attachment.file.name)[1].lower()
-        is_pdf = file_extension == '.pdf'
-
-        context = {
-            'attachment': attachment,
-            'is_pdf': is_pdf,
-            'pageTitle': f"Viewing - {attachment.name}",
-        }
-
-        return django.shortcuts.render(request, 'documents/attachment_file_view.html', context)
-
-    except documents.models.Attachment.DoesNotExist:
-        return django.http.HttpResponseNotFound("Attachment not found")
